b25cc36972cdf9933bc19bba86a895399459b04c
Oleksandra Kuziura <radioday2014@gmail.com>
2025-01-23 14:52:52 +0200

    remove back tip oneventName in eventlisener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 function waitFor(element, eventName) {
   return new Promise((resolve, reject) => {
-    element.addEventListener(`${eventName}`, () => {
+    element.addEventListener(eventName, () => {
       resolve(
         `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
       );
